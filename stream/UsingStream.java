50ef2a9115f3ab35adaec9f88e0e7633a2d9cc1e
TAMILMANI T <70768934+Tamilmani2801@users.noreply.github.com>
2023-10-04 12:35:12 +0530

    Stream

---
+++ stream/UsingStream.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class UsingStream
+{
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(3,4,5,6,7,8,9);
+
+        int result = list.stream().filter(n->n%2==0).map(n->n*2).reduce(0,(c,e)->c+e);
+
+        System.out.println(result);
+        
+    }
+}
