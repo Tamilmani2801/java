6f1b078362c64ece5c82bd56a5f319508e376032
TAMILMANI T <70768934+Tamilmani2801@users.noreply.github.com>
2023-10-28 15:15:04 +0530

    Delete learning/WEB-INF directory

--- learning/WEB-INF/classes/Bike.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Bike implements Vehicle
-{
-    public void drive()
-    {
-        System.out.println("ride the bike");
-    }   
-}
--- learning/WEB-INF/classes/Car.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Car implements Vehicle
-{
-    public void drive()
-    {
-        System.out.println("we will drive car..");
-    }   
-}
